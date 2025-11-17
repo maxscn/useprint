30a9a61ae6d760003a98db7bea23f9328e79024d
Maximilian Schön <maximilian.ak.schon@gmail.com>
2025-11-17 12:00:42 +0100

    fix pagebreak

---
+++ .changeset/vast-sides-check.md
@@ -0,0 +1,5 @@
+---
+"@skrift/components": patch
+---
+
+Fix pagebreak for real.
--- packages/components/src/new-page.tsx
+++ packages/components/src/new-page.tsx
@@ -3,6 +3,7 @@ import type React from "react";
 export const NewPage: React.FC = () => {
 	const unbreakableStyle: React.CSSProperties = {
 		breakAfter: "all",
+		pageBreakAfter: "always"
 	};
 
 	return <div style={unbreakableStyle} />;
