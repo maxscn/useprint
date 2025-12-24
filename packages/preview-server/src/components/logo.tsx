8b81a09ea3653152c2ba055af271f2417c1db8da
Maximilian Schön <maximilian.ak.schon@gmail.com>
2025-12-24 12:35:34 +0100

    icon

--- packages/preview-server/src/components/logo.tsx
+++ packages/preview-server/src/components/logo.tsx
@@ -5,9 +5,14 @@ import logo from "../app/logo.png";
 export const Logo = () => {
 	return (
 		<div className="flex flex-col items-start justify-center py-4 px-6">
-		<span className="text-white">
+		<span className="flex flex-row items-center  gap-2 text-white">
 
-		  useprint.dev
+		<svg width="24" height="24" viewBox="0 0 1000 1043" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="1000" height="1042.55"/>
+<path d="M276.594 425.53V177.303M702.126 425.53V177.303M843.97 744.679V425.53M134.75 744.679V425.53M276.594 886.523V602.835M702.126 886.523V602.835" stroke="white" stroke-width="70.922"/>
+</svg>
+useprint.dev
+
 		</span>
 		</div>
 	);
